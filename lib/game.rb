cafaf808a945cf67af77d0644c2ce0f575b5ea0e
RyanDBarnett <conner2468@gmail.com>
2020-07-22 16:52:48 -0600

    Update Game convert nums from strings to ints

--- lib/game.rb
+++ lib/game.rb
@@ -14,14 +14,14 @@ class Game
               :venue_link
 
   def initialize(args)
-    @game_id      = args[:game_id]
-    @season       = args[:season]
+    @game_id      = args[:game_id].to_i
+    @season       = args[:season].to_i
     @type         = args[:type]
     @date_time    = args[:date_time]
-    @away_team_id = args[:away_team_id]
-    @home_team_id = args[:home_team_id]
-    @away_goals   = args[:away_goals]
-    @home_goals   = args[:home_goals]
+    @away_team_id = args[:away_team_id].to_i
+    @home_team_id = args[:home_team_id].to_i
+    @away_goals   = args[:away_goals].to_i
+    @home_goals   = args[:home_goals].to_i
     @venue        = args[:venue]
     @venue_link   = args[:venue_link]
   end
