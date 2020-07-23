9ff0e0975642a3f64ceb4657834be7a2480a5699
RyanDBarnett <conner2468@gmail.com>
2020-07-23 15:59:35 -0600

    Update highest total score test with mocks and stubs

--- test/game_statistics_test.rb
+++ test/game_statistics_test.rb
@@ -1,24 +1,31 @@
 require 'minitest/autorun'
 require 'minitest/pride'
+require 'mocha/minitest'
 require './lib/stat_tracker'
 
+MockGame = Struct.new(:home_goals, :away_goals)
 
 class GameStatisticsTest < Minitest::Test
   def setup
-    game_path = './test/fixtures/games.csv'
-    team_path = './data/teams.csv'
-    game_teams_path = './test/fixtures/game_teams.csv'
-
     locations = {
-      games: game_path,
-      teams: team_path,
-      game_teams: game_teams_path
+      games: 'no path',
+      teams: 'no path',
+      game_teams: 'no path'
     }
 
     @stat_tracker = StatTracker.from_csv(locations)
   end
 
   def test_highest_total_score
-    assert_equal 8, @stat_tracker.highest_total_score
+    game1 = MockGame.new(4, 6)
+    game2 = MockGame.new(2, 2)
+    game3 = MockGame.new(0, 3)
+    @stat_tracker.stubs(:games).returns([
+      game1,
+      game2,
+      game3
+    ])
+
+    assert_equal 10, @stat_tracker.highest_total_score
   end
 end
