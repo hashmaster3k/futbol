931437bca6aece7f0c859875ac66a9dc9f00fa63
Daniel Halverson <daniel.halverson@hotmail.com>
2020-07-22 18:05:21 -0600

    Add league stats count of games by team id

--- lib/league_statistics.rb
+++ lib/league_statistics.rb
@@ -4,7 +4,11 @@ module LeagueStatistics
     teams.count
   end
 
-
+  def count_of_games_by_team_id(team_id)
+    game_teams.find_all do |game_team|
+      game_team.team_id == team_id
+    end.count
+  end
 
 
 
--- test/league_statistics_test.rb
+++ test/league_statistics_test.rb
@@ -22,7 +22,16 @@ class LeagueStatisticsTest < Minitest::Test
   #   assert_equal 8, @stat_tracker.highest_total_score
   # end
 
-  def test_count_of_teams
+  def test_it_can_return_count_of_teams
     assert_equal 32, @stat_tracker.count_of_teams
   end
+
+  def test_it_can_return_count_of_games_by_team_id
+    assert_equal 5, @stat_tracker.count_of_games_by_team_id(19)
+  end
+
+  # def test_it_can_return_best_offense
+  #   assert_equal "Reign FC", @stat_tracker.best_offense
+  # end
+
 end
