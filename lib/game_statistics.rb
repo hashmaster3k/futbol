34e047d87f310c230bc82907da9766ec58de1c33
RyanDBarnett <conner2468@gmail.com>
2020-07-23 17:24:22 -0600

    Add percentage home wins and select by key values methods

--- lib/game_statistics.rb
+++ lib/game_statistics.rb
@@ -8,4 +8,18 @@ module GameStatistics
       game.home_goals + game.away_goals
     end
   end
+
+  def percentage_home_wins
+    home_games = select_by_key_value(game_teams, :hoa, 'home')
+
+    won_home_games = select_by_key_value(home_games, :result, 'WIN')
+
+    (won_home_games.length.to_f / home_games.length).round(2)
+  end
+
+  def select_by_key_value(array, key, value)
+    array.select do |element|
+      element[key] == value
+    end
+  end
 end
