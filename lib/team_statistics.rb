9647d09a4301d3c152ccda6afc5f4de70dfe207d
Hashim Gari <hashmaster3k@gmail.com>
2020-07-24 09:39:50 -0600

    Creates team statistics class and test file

---
+++ lib/team_statistics.rb
@@ -0,0 +1,2 @@
+module TeamStatistics
+end
---
+++ test/team_statistics_test.rb
@@ -0,0 +1,24 @@
+require './lib/team_statistics'
+require './lib/stat_tracker'
+require 'minitest/autorun'
+require 'minitest/pride'
+
+class TeamStatisticsTest < Minitest::Test
+
+  def setup
+    game_path = './data/games.csv'
+    team_path = './data/teams.csv'
+    game_teams_path = './data/game_teams.csv'
+
+    locations = {games: game_path,
+                 teams: team_path,
+                 game_teams: game_teams_path}
+
+    @stat_tracker = StatTracker.from_csv(locations)
+  end
+
+  def test_it_exists
+    assert_instance_of StatTracker, @stat_tracker
+  end
+
+end
