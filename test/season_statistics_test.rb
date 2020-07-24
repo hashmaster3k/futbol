d3fee60c7ac6643eca1f8e514e73356ea1350aad
Hashim Gari <hashmaster3k@gmail.com>
2020-07-24 09:24:19 -0600

    Changed name from stat_tracker_test to season_statistic_test

---
+++ test/season_statistics_test.rb
@@ -0,0 +1,54 @@
+require './lib/season_statistics'
+require './lib/stat_tracker'
+require 'minitest/autorun'
+require 'minitest/pride'
+
+class SeasonStatisticsTest < Minitest::Test
+
+  def setup
+    game_path = './data/games.csv'
+    team_path = './data/teams.csv'
+    game_teams_path = './data/game_teams.csv'
+
+    locations = {games: game_path,
+                 teams: team_path,
+                 game_teams: game_teams_path}
+
+    @stat_tracker = StatTracker.from_csv(locations)
+  end
+
+  def test_it_exists
+    assert_instance_of StatTracker, @stat_tracker
+  end
+
+  def test_winningest_coach
+    assert_equal "Claude Julien", @stat_tracker.winningest_coach("20132014")
+    assert_equal "Alain Vigneault", @stat_tracker.winningest_coach("20142015")
+  end
+
+  def test_worst_coach
+    assert_equal "Peter Laviolette", @stat_tracker.worst_coach("20132014")
+    assert_equal "Ted Nolan", @stat_tracker.worst_coach("20142015")
+  end
+
+  def test_most_accurate
+    assert_equal "Real Salt Lake", @stat_tracker.most_accurate_team("20132014")
+    assert_equal "Toronto FC", @stat_tracker.most_accurate_team("20142015")
+  end
+
+  def test_least_accurate
+    assert_equal "New York City FC", @stat_tracker.least_accurate_team("20132014")
+    assert_equal "Columbus Crew SC", @stat_tracker.least_accurate_team("20142015")
+  end
+
+  def test_most_tackles
+    assert_equal "FC Cincinnati", @stat_tracker.most_tackles("20132014")
+    assert_equal "Seattle Sounders FC", @stat_tracker.most_tackles("20142015")
+  end
+
+  def test_fewest_tackles
+    assert_equal "Atlanta United", @stat_tracker.fewest_tackles("20132014")
+    assert_equal "Orlando City SC", @stat_tracker.fewest_tackles("20142015")
+  end
+
+end
